20eb966481012a3481ceeeb45eaaa093e44ad6ee
Adam Lyskawa <adam@codedog.pl>
2021-12-06 22:40:00 +0100

    FIX: Default value conversions.

--- Packages/Woof.Config/ValueConversions.cs
+++ Packages/Woof.Config/ValueConversions.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Defines from string and to string conversions for types.
 /// </summary>
-public class ValueConversions : Dictionary<Type, (ValueParser Parse, ValueToString GetString)> {
+public class ValueConversions : Dictionary<Type, (ValueParser Parse, ValueToString GetString, bool IsQuoted)> {
 
     /// <summary>
     /// Gets the value conversions for the basic system types.
@@ -14,28 +14,28 @@ public class ValueConversions : Dictionary<Type, (ValueParser Parse, ValueToStri
     /// <see cref="T:byte[]"/> is expressed as Base64 encoded string.
     /// </remarks>
     public static ValueConversions Default { get; } = new() {
-        [typeof(string)] = (value => value, value => (string)value),
-        [typeof(bool)] = (value => bool.Parse(value), value => ((bool)value).ToString()),
-        [typeof(int)] = (value => int.Parse(value, N), value => ((bool)value).ToString(N)),
-        [typeof(double)] = (value => double.Parse(value, N), value => ((double)value).ToString(N)),
-        [typeof(decimal)] = (value => decimal.Parse(value, N), value => ((decimal)value).ToString(N)),
-        [typeof(DateTime)] = (value => DateTime.Parse(value, N), value => ((DateTime)value).ToString("s")),
-        [typeof(TimeSpan)] = (value => TimeSpan.FromSeconds(double.Parse(value, N)), value => ((TimeSpan)value).TotalSeconds.ToString(N)),
-        [typeof(byte[])] = (value => Convert.FromBase64String(value), value => Convert.ToBase64String((byte[])value)),
-        [typeof(float)] = (value => float.Parse(value, N), value => ((float)value).ToString(N)),
-        [typeof(byte)] = (value => byte.Parse(value, N), value => ((byte)value).ToString(N)),
-        [typeof(short)] = (value => short.Parse(value, N), value => ((short)value).ToString(N)),
-        [typeof(long)] = (value => long.Parse(value, N), value => ((long)value).ToString(N)),
-        [typeof(sbyte)] = (value => sbyte.Parse(value, N), value => ((sbyte)value).ToString(N)),
-        [typeof(ushort)] = (value => ushort.Parse(value, N), value => ((ushort)value).ToString(N)),
-        [typeof(uint)] = (value => uint.Parse(value, N), value => ((uint)value).ToString(N)),
-        [typeof(ulong)] = (value => ulong.Parse(value, N), value => ((ulong)value).ToString(N)),
-        [typeof(DateOnly)] = (value => DateOnly.Parse(value, N), value => ((DateOnly)value).ToString("s")),
-        [typeof(TimeOnly)] = (value => TimeOnly.Parse(value, N), value => ((TimeOnly)value).ToString("s")),
-        [typeof(Uri)] = (value => new Uri(value), value => ((Uri)value).ToString()),
-        [typeof(FileInfo)] = (value => new FileInfo(value), value => ((FileInfo)value).ToString()),
-        [typeof(DirectoryInfo)] = (value => new DirectoryInfo(value), value => ((DirectoryInfo)value).ToString()),
-        [typeof(Guid)] = (value => new Guid(value), value => ((Guid)value).ToString()),
+        [typeof(string)] = (value => value, value => (string)value, true),
+        [typeof(bool)] = (value => bool.Parse(value), value => ((bool)value).ToString(), false),
+        [typeof(int)] = (value => int.Parse(value, N), value => ((int)value).ToString(N), false),
+        [typeof(double)] = (value => double.Parse(value, N), value => ((double)value).ToString(N), false),
+        [typeof(decimal)] = (value => decimal.Parse(value, N), value => ((decimal)value).ToString(N), false),
+        [typeof(DateTime)] = (value => DateTime.Parse(value, N), value => ((DateTime)value).ToString("s"), true),
+        [typeof(TimeSpan)] = (value => TimeSpan.FromSeconds(double.Parse(value, N)), value => ((TimeSpan)value).TotalSeconds.ToString(N), false),
+        [typeof(byte[])] = (value => Convert.FromBase64String(value), value => Convert.ToBase64String((byte[])value), true),
+        [typeof(float)] = (value => float.Parse(value, N), value => ((float)value).ToString(N), false),
+        [typeof(byte)] = (value => byte.Parse(value, N), value => ((byte)value).ToString(N), false),
+        [typeof(short)] = (value => short.Parse(value, N), value => ((short)value).ToString(N), false),
+        [typeof(long)] = (value => long.Parse(value, N), value => ((long)value).ToString(N), false),
+        [typeof(sbyte)] = (value => sbyte.Parse(value, N), value => ((sbyte)value).ToString(N), false),
+        [typeof(ushort)] = (value => ushort.Parse(value, N), value => ((ushort)value).ToString(N), false),
+        [typeof(uint)] = (value => uint.Parse(value, N), value => ((uint)value).ToString(N), false),
+        [typeof(ulong)] = (value => ulong.Parse(value, N), value => ((ulong)value).ToString(N), false),
+        [typeof(DateOnly)] = (value => DateOnly.Parse(value, N), value => ((DateOnly)value).ToString("yyyy-MM-dd"), true),
+        [typeof(TimeOnly)] = (value => TimeOnly.Parse(value, N), value => ((TimeOnly)value).ToString("HH:mm:ss.fff"), true),
+        [typeof(Uri)] = (value => new Uri(value), value => ((Uri)value).ToString(), true),
+        [typeof(FileInfo)] = (value => new FileInfo(value), value => ((FileInfo)value).ToString(), true),
+        [typeof(DirectoryInfo)] = (value => new DirectoryInfo(value), value => ((DirectoryInfo)value).ToString(), true),
+        [typeof(Guid)] = (value => new Guid(value), value => ((Guid)value).ToString(), true),
     };
 
     /// <summary>

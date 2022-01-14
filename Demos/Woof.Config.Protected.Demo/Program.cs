df18a524678cab905307aad5511a2ad802a938c8
Adam Lyskawa <adam@codedog.pl>
2022-01-14 15:32:11 +0100

    REMOVE: Legacy demo.

--- Demos/Woof.Config.Protected.Demo/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Woof.DataProtection;
-
-var json = new JsonConfigProtected(DataProtectionScope.CurrentUser).Protect();
-if (!json.IsProtected) Console.WriteLine("Data protection failed.");
-//var config = json.Get<AppConfiguration>();
-//Console.WriteLine($"Login: {config.Login}, ApiKey: {config.ApiKey}");

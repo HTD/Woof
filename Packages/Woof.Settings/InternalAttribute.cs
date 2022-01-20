f002b995c42bd7ef978e02e1575b12f6502b562a
Adam Lyskawa <adam@codedog.pl>
2022-01-20 16:21:48 +0100

    ADD: Internal attribute.

---
+++ Packages/Woof.Settings/InternalAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace Woof.Settings;
+
+/// <summary>
+/// Property with this adnotation is used internally and should be ignored while loading and saving the configuration data.
+/// </summary>
+public class InternalAttribute : SpecialAttribute { }

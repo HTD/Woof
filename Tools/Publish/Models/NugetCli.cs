0fca5127979a4e1856c0f5c0a7b5436c0924b208
Adam Lyskawa <adam@codedog.pl>
2022-01-22 23:03:20 +0100

    ADD: ResetRepository() and XML documentation.

--- Tools/Publish/Models/NugetCli.cs
+++ Tools/Publish/Models/NugetCli.cs
@@ -1,18 +1,33 @@
 ﻿namespace Publish.Models;
 
-public class NugetCli {
+/// <summary>
+/// Contains NuGet CLI helpers.
+/// </summary>
+public static class NugetCli {
 
-    public const string DownloadLink = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
-
+    /// <summary>
+    /// Creates or reloads the local NuGet package repository.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> completed when the repository is created or updated.</returns>
     public static async ValueTask ReloadRepositoryAsync() {
-        var root = Path.GetFullPath(Settings.Default.Paths.Root);
-        var source = Path.Combine(root, Settings.Default.Paths.PackageBinaries);
-        var target = Path.Combine(root, Settings.Default.Paths.Repo);
-        var command = new ShellCommand($"nuget init \"{source}\" \"{target}\"");
+        Directory.CreateDirectory(Target);
+        var command = new ShellCommand($"nuget init \"{Source}\" \"{Target}\"");
         await EnsureAvailableAsync();
         await command.ExecVoidAsync();
     }
 
+    /// <summary>
+    /// Resets (deletes) the local NuGet package repository.
+    /// </summary>
+    public static void ResetRepository() {
+        Directory.Delete(Target, recursive: true);
+        Directory.CreateDirectory(Target);
+    }
+
+    /// <summary>
+    /// Ensures the NuGet CLI tool is available. If not, it's downloaded.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> completed when nuget command is tested or downloaded.</returns>
     private static async ValueTask EnsureAvailableAsync() {
         var nuget = new ShellCommand("nuget");
         try {
@@ -23,6 +38,10 @@ public class NugetCli {
         }
     }
 
+    /// <summary>
+    /// Downloads the NuGet CLI tool.
+    /// </summary>
+    /// <returns>A <see cref="ValueTask"/> completed when the nuget command is downloaded.</returns>
     private static async ValueTask DownloadAsync() {
         using var httpClient = new HttpClient();
         using var response = await httpClient.GetAsync(DownloadLink);
@@ -30,5 +49,24 @@ public class NugetCli {
         await using var fileStream = new FileStream("nuget.exe", FileMode.Create, FileAccess.Write, FileShare.None);
         await responseStream.CopyToAsync(fileStream);
     }
+
+    #region Static configuration
+
+    /// <summary>
+    /// Gets the necessary configuration values from the program settings on first access.
+    /// </summary>
+    static NugetCli() {
+        if (!Settings.Default.IsLoaded) Settings.Default.Load();
+        Root = Path.GetFullPath(Settings.Default.Paths.Root);
+        Source = Path.Combine(Root, Settings.Default.Paths.PackageBinaries);
+        Target = Path.Combine(Root, Settings.Default.Paths.Repo);
+    }
+
+    private const string DownloadLink = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe";
+    private static readonly string Root;
+    private static readonly string Source;
+    private static readonly string Target;
+
+    #endregion
 
 }

24a94d7842652d987166d0b940b07f9e976cf0cf
Adam Lyskawa <adam@codedog.pl>
2022-01-22 23:08:44 +0100

    FIX: Formatting.

--- Tools/Publish/Views/MainWindow.xaml.cs
+++ Tools/Publish/Views/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System.Windows;
+﻿namespace Publish.Views;
 
-namespace Publish.Views;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+
     public MainWindow() => InitializeComponent();
+
 }

80f720d52f1819b17ee129173ab5c4c916197afe
Adam Lyskawa <adam@codedog.pl>
2022-01-26 19:16:11 +0100

    UPDATE.

--- Packages/Woof.Template/Roadmap.cs
+++ Packages/Woof.Template/Roadmap.cs
@@ -1,9 +1,6 @@
 ﻿// TODO: 1. Test the service installer for the data protection usage.
-// TODO: 2. Set all package versions to 6.2.0-alpha.20
+// TODO: 2. Set all package versions to 6.2.0.
 // TODO: 3. Fix documentation issues (internal / external links).
-// TODO: 4. Fix projects metadata:
-// TODO: 5.1. Publish packages with no dependencies.
-// TODO: 5.2. Publish the rest of the packages in order of least dependencies with setting dependencies to already published packages.
+// TODO: 4. Fix projects metadata - check repository links!
 // TODO: 6. Include tests for all applicable classes.
 // TODO: 7. Include demos for all packages.
-// LATER: Add Windows MVVM packages

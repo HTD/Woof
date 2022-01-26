c874c7396c6ce146f32dd9caddfca356b9bcd813
Adam Lyskawa <adam@codedog.pl>
2022-01-26 19:07:56 +0100

    ADD: Documentation, clearing the items on reload.

--- Tools/WoofRepositoryManager/Models/PackageCollectionTraits.cs
+++ Tools/WoofRepositoryManager/Models/PackageCollectionTraits.cs
@@ -1,11 +1,25 @@
 ﻿namespace WoofRepositoryManager.Models;
 
+/// <summary>
+/// Extension methods for NuGet package collections.
+/// </summary>
 public static class PackageCollectionTraits {
 
+    /// <summary>
+    /// Gets all packages (including local dependencies) in the collection in DFS order.
+    /// </summary>
+    /// <param name="items">Packages with dependencies.</param>
+    /// <returns>Packages with dependencies.</returns>
     public static IEnumerable<PackageNode> All(this ObservableList<PackageNode> items) => items.TraverseDFS(i => i.Dependencies);
 
+    /// <summary>
+    /// Gets the packages from the local repository.
+    /// </summary>
+    /// <param name="items">Target list to fill asynchronously.</param>
+    /// <returns>A <see cref="ValueTask"/> completed when the all the packages are fetched from the local repository.</returns>
     public static async ValueTask GetAsync(this ObservableList<PackageNode> items) {
-        await foreach (var package in new LocalRepository().GetPackagesAsync()) {
+        items.Clear();
+        await foreach (var package in LocalRepository.GetPackagesAsync()) {
             var lastVersion = (await package.GetVersionsAsync())?.Max(i => i.Version)?.ToString();
             var name = package.Identity.Id;
             var version = lastVersion;
@@ -30,6 +44,11 @@ public static class PackageCollectionTraits {
         }
     }
 
+    /// <summary>
+    /// Gets all the packages (including local dependencies) that are currently checked.
+    /// </summary>
+    /// <param name="items">Packages bound to the view.</param>
+    /// <returns>All package items that are checked in the view.</returns>
     public static IEnumerable<PackageItem> GetChecked(this IEnumerable<PackageNode> items) {
         List<PackageNode> result = new();
         foreach (var item in items.TraverseDFS(n => n.Dependencies).Where(i => i.IsChecked))

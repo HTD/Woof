fd5bcdebd7213ea7ad7905fb0e23557b4b8001ce
Adam Lyskawa <adam@codedog.pl>
2022-01-26 21:48:16 +0100

    ADD: System.Text reference.

--- Packages/Woof.Internals/.global.cs
+++ Packages/Woof.Internals/.global.cs
@@ -3,6 +3,7 @@ global using System.Diagnostics;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Runtime.InteropServices;
+global using System.Text;
 global using System.Xml;
 global using Woof.DotNet;
 global using Woof.Internals;

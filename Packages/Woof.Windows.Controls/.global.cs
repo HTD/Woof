27c7465c0d446f2ee807c6182e36efc8f76be5e8
Adam Lyskawa <adam@codedog.pl>
2022-01-27 10:51:24 +0100

    ADD: Global usings.

---
+++ Packages/Woof.Windows.Controls/.global.cs
@@ -0,0 +1,11 @@
+﻿global using System.Collections;
+global using System.Collections.ObjectModel;
+global using System.Collections.Specialized;
+global using System.ComponentModel;
+global using System.Reflection;
+global using System.Windows;
+global using System.Windows.Controls;
+global using System.Windows.Media;
+global using System.Windows.Media.Animation;
+
+global using Woof.Windows.Mvvm;

a6053a55a0f62d5336a17d1633ac4f403c837e97
Adam Lyskawa <adam@codedog.pl>
2022-02-04 23:59:14 +0100

    REMOVE: Tester project.

--- Tools/Tester/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Woof.LinuxAdmin;
-
-var serviceUser = UserInfo.FromName("service");
-var path = "~/.net/dpapi";
-var resolved = Linux.ResolveUserPath(path, serviceUser!);
-Console.WriteLine(resolved);

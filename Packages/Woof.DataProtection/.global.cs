f757ef7f89bf65dbc17685e05df7acd77ae3b51d
Adam Lyskawa <adam@codedog.pl>
2022-02-20 01:03:11 +0100

    FIX: Remove non-admin permission from service DPAPI directory.

--- Packages/Woof.DataProtection/.global.cs
+++ Packages/Woof.DataProtection/.global.cs
@@ -1,7 +1,7 @@
 ﻿global using System.Runtime.InteropServices;
+global using System.Security.AccessControl;
 global using System.Security.Cryptography;
 global using System.Text;
-
 global using Microsoft.AspNetCore.DataProtection;
 global using Microsoft.AspNetCore.DataProtection.KeyManagement;
 global using Microsoft.Extensions.DependencyInjection;
--- Packages/Woof.DataProtection/Api/WindowsLocalSystemKey.cs
+++ Packages/Woof.DataProtection/Api/WindowsLocalSystemKey.cs
@@ -13,13 +13,27 @@ public class WindowsLocalSystemKey : DataProtectionKeyBase {
     /// </summary>
     public WindowsLocalSystemKey() : base(GetConfiguration()) { }
 
+
     /// <summary>
     /// Gets the configuration for the base constructor.
     /// </summary>
     /// <returns>Data protection key configuration.</returns>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Used only for Windows")]
     private static DataProtectionKeyConfiguration GetConfiguration() {
         var systemDirectory = Environment.GetFolderPath(Environment.SpecialFolder.System);
         var target = Path.Combine(systemDirectory, "DPAPI");
+        var targetDirectory = new DirectoryInfo(target);
+        if (!targetDirectory.Exists) {
+            targetDirectory.Create();
+            targetDirectory = new DirectoryInfo(target);
+            var targetSecurity = new DirectorySecurity();
+            var acl = targetSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+            targetSecurity.SetAccessRuleProtection(true, false);
+            foreach (FileSystemAccessRule ace in acl) targetSecurity.PurgeAccessRules(ace.IdentityReference);
+            targetSecurity.AddAccessRule(new FileSystemAccessRule("SYSTEM", FileSystemRights.FullControl, AccessControlType.Allow));
+            targetSecurity.AddAccessRule(new FileSystemAccessRule("CREATOR OWNER", FileSystemRights.FullControl, AccessControlType.Allow));
+            targetDirectory.SetAccessControl(targetSecurity);
+        }
         var purpose = "Woof.DPAPI:Local System Service Key";
         return GetConfiguration(target, purpose);
     }

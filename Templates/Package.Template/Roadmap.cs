7d3efc296127c68cdc6e01efdcec3242da2120aa
Adam Lyskawa <adam@codedog.pl>
2022-02-20 13:29:10 +0100

    6.2.0 RELEASE

--- Templates/Package.Template/Roadmap.cs
+++
@@ -1,6 +0,0 @@
-﻿// TODO: 1. Test the service installer for the data protection usage.
-// TODO: 2. Set all package versions to 6.2.0.
-// TODO: 3. Fix documentation issues (internal / external links).
-// TODO: 4. Fix projects metadata - check repository links!
-// TODO: 6. Include tests for all applicable classes.
-// TODO: 7. Include demos for all packages.
